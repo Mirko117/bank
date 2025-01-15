56dc34bdbfe3651150bf6946754a07e2120d3987
Mirko117 <suvajac117@gmail.com>
2025-01-15 21:58:55 +0100

    Added changeable shell in dashboard

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,12 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, render_template, make_response
 from flask_restx import Api, Resource, Namespace
 from app.models import User
 
 
-# Create blueprint, API and namespace
+# Create blueprint, API and namespace for API
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp, title="Main API", version="1.0", description="Endpoints for main API")
 api_ns = Namespace("api", description="Main operations")
+
+# Create namespace for dashboard
+dashbpard_ns = Namespace("dashboard", description="Dashboard operations")
 
 
 @api_ns.route('/users')
@@ -16,5 +19,22 @@ class UsersEndpoint(Resource):
         return jsonify([user.serialize() for user in users])
 
 
-# Add namespace to API
+@dashbpard_ns.route('/get-shell')
+class DashboardGetPageEndpoint(Resource):
+    def get(self):
+        shell = request.args.get('shell')
+        
+        if not shell:
+            response = make_response({"status": "error", "message": "No shell provided"}, 400)
+            return response
+
+        # Render the shell
+        rendered_shell = render_template(f"dashboard/shells/{shell}.html")
+
+        # Return the rendered shell
+        response = make_response({"status": "success", "shell": rendered_shell}, 200)
+        return response
+
+# Add namespaces to API
 api.add_namespace(api_ns, path='/')
+api.add_namespace(dashbpard_ns, path='/dashboard')
--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,10 +1,23 @@
+
+
 $(document).ready(function() {
     $("#sidebar a").on("click", function(e) {
         e.preventDefault();
         $("#sidebar a").removeClass("selected");
         this.classList.add("selected");
 
-        var url = this.href;
-        
+        var shell = this.getAttribute("data-shell");
+        var data = { shell: shell };
+        var text = this.text;
+
+        $.ajax({
+            type: "GET",
+            url: "/api/dashboard/get-shell",
+            data: data,
+            success: function (response) {
+                $("#shell").html(response.shell);
+                $("#navbar .page-name").text(text);
+            }
+        });
     });
 });
